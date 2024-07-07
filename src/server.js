e2d0ef4ad0aa32a4105b9c8fe9e804a75ee8214a
Aryash Dubey <dubeyaryash@gmail.com>
2024-07-07 01:35:13 -0700

    pushed server changes

--- src/server.js
+++ src/server.js
@@ -83,7 +83,7 @@ app.post("/convert", async (req, res) => {
         firstPageToConvert: firstPage,
         lastPageToConvert: lastPage,
         pngFile: true,
-        scalePageTo: 1024,
+        scalePageTo: totalPages > 40 ? 1024 : 1536,
       };
 
       await convertPDFChunk(tempFile, outputFileName, options);
